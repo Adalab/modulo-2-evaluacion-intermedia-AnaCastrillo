3d2275eb36b7876f88cb391922e8bf5e5964579d
AnaCastrillo <callmeferre@gmail.com>
2020-10-13 09:29:42 +0200

    added constants

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ function getRandomNumber(max) {
 const randomN = getRandomNumber(100);
 console.log("El número aleatorio es " + randomN);
 
+const text = document.querySelector(".text");
+const hint = document.querySelector(".hint");
+const attempt = document.querySelector(".attempt");
+const btn = document.querySelector(".btn");
