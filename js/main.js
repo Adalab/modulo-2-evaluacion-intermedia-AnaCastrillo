3e51ad3c8de49621dc06f30ada5954e045aa9a5d
AnaCastrillo <callmeferre@gmail.com>
2020-10-13 10:08:57 +0200

    prevent default submit in form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
-  }
+	return Math.ceil(Math.random() * max);
+}
 
 const randomN = getRandomNumber(100);
 console.log("El número aleatorio es " + randomN);
@@ -12,20 +12,28 @@ let attemptN = parseInt(attempt.innerHTML);
 const btn = document.querySelector(".btn");
 
 function tryN() {
-  const textN = text.value;
-  console.log("Prueba con " + textN)
-  if (textN < 100 && textN > 1){
-    if (textN < randomN){
-      hint.innerHTML = "Demasiado bajo";
-    } else if (textN > randomN){
-      hint.innerHTML = "Demasiado alto";
-    }else {
-      hint.innerHTML = "¡Has ganado campeona!";
-    }
-  }else {
-    hint.innerHTML = "El número ebe estar comprendido entre 1 y 100";
-  }
-  attempt.innerHTML = ++attemptN;
+	const textN = text.value;
+	console.log("Prueba con " + textN);
+	if (textN < 100 && textN > 1) {
+		if (textN < randomN) {
+			hint.innerHTML = "Demasiado bajo";
+		} else if (textN > randomN) {
+			hint.innerHTML = "Demasiado alto";
+		} else {
+			hint.innerHTML = "¡Has ganado campeona!";
+		}
+	} else {
+		hint.innerHTML = "El número ebe estar comprendido entre 1 y 100";
+	}
+	attempt.innerHTML = ++attemptN;
 }
 
 btn.addEventListener("click", tryN);
+
+const form = document.querySelector(".js-form");
+
+function handleForm(ev) {
+	ev.preventDefault();
+}
+
+form.addEventListener("submit", handleForm);
